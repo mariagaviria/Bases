17e331aa794853a1cf5a09014d4f54643923069e
mariagaviria <44762498+mariagaviria@users.noreply.github.com>
2020-03-12 16:16:47 -0500

    Update ScriptCreacionTablas.sql

--- Banco/ScriptCreacionTablas.sql
+++ Banco/ScriptCreacionTablas.sql
@@ -1,8 +1,8 @@
 create table cliente (
   cliente_cc varchar(30),
-  cliente_apellido1 varchar(30),
+  cliente_apellido1 varchar(30) NOT NULL,
   cliente_apellido2 varchar(30),
-  cliente_nombre1 varchar(30),
+  cliente_nombre1 varchar(30) NOT NULL,
   cliente_nombre2 varchar(30),
   cliente_telefono numeric(15,0),
   cliente_email varchar(100),
@@ -12,7 +12,6 @@ create table cliente (
 create table producto(
   producto_ID integer,
   producto_nombre varchar(20),
-  cupo numeric(12,2),
   primary key (producto_ID)
 );
  
@@ -20,7 +19,8 @@ create table productosDeCliente(
   producto_numero varchar(10),
   cliente_cc varchar(30),
   producto_id integer,
-  saldo numeric(18,2),
+  saldo numeric(18,2) CHECK (saldo >= cupo) NOT NULL,
+  cupo numeric(10,2) CHECK (cupo <= 0),
   primary key (producto_numero),
   foreign key (producto_ID) references producto, 
   foreign key (cliente_cc) references cliente
