836ff08da271aec7d6e3d3a3ed00bd00488379e4
mariagaviria <44762498+mariagaviria@users.noreply.github.com>
2020-03-30 19:29:50 -0500

    Update VentasPorLocalidad.sql

--- Mercado/VentasPorLocalidad.sql
+++ Mercado/VentasPorLocalidad.sql
@@ -1,23 +1,31 @@
 SELECT * FROM factura
 WHERE punto_de_venta_localidad = 'RAFAEL URIBE URIBE'
+--Cantidad: 1
 
 SELECT * FROM factura
 WHERE punto_de_venta_localidad = 'CIUDAD BOLIVAR'
+--Cantidad: 2
 
 SELECT * FROM factura
 WHERE punto_de_venta_localidad = 'BOSA'
+--Cantidad: 2
 
 SELECT * FROM factura
 WHERE punto_de_venta_localidad = 'PUENTE ARANDA'
+--Cantidad: 2
 
 SELECT * FROM factura
 WHERE punto_de_venta_localidad = 'USME'
+--Cantidad: 1
 
 SELECT * FROM factura
 WHERE punto_de_venta_localidad = 'SUBA'
+--Cantidad: 3
 
 SELECT * FROM factura
 WHERE punto_de_venta_localidad = 'CHAPINERO'
+--Cantidad: 0
 
 SELECT * FROM factura
 WHERE punto_de_venta_localidad = 'BARRIOS UNIDOS'
+--Cantidad: 0
