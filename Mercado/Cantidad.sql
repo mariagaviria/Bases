bc8af1a28037c0cc357b9e5c9a91596cb06fccfb
mariagaviria <44762498+mariagaviria@users.noreply.github.com>
2020-03-30 19:58:06 -0500

    Create Cantidad.sql

---
+++ Mercado/Cantidad.sql
@@ -0,0 +1,14 @@
+SELECT SUM(cantidad)
+FROM productosdefactura
+WHERE nom_producto = 'DETERGENTE'
+--Unidades vendidas: 21
+
+SELECT SUM(cantidad)
+FROM productosdefactura
+WHERE nom_producto = 'JABON LOZA'
+--Unidades vendidas: 30
+
+SELECT SUM(cantidad)
+FROM productosdefactura
+WHERE nom_producto = 'JABON MANOS'
+--Unidades vendidas: 12
