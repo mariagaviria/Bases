8518b45c2558692d2ca5aa6561981cf36e05a332
mariagaviria <44762498+mariagaviria@users.noreply.github.com>
2020-03-30 19:24:50 -0500

    Create VentasPorLocalidad.sql

---
+++ Mercado/VentasPorLocalidad.sql
@@ -0,0 +1,23 @@
+SELECT * FROM factura
+WHERE punto_de_venta_localidad = 'RAFAEL URIBE URIBE'
+
+SELECT * FROM factura
+WHERE punto_de_venta_localidad = 'CIUDAD BOLIVAR'
+
+SELECT * FROM factura
+WHERE punto_de_venta_localidad = 'BOSA'
+
+SELECT * FROM factura
+WHERE punto_de_venta_localidad = 'PUENTE ARANDA'
+
+SELECT * FROM factura
+WHERE punto_de_venta_localidad = 'USME'
+
+SELECT * FROM factura
+WHERE punto_de_venta_localidad = 'SUBA'
+
+SELECT * FROM factura
+WHERE punto_de_venta_localidad = 'CHAPINERO'
+
+SELECT * FROM factura
+WHERE punto_de_venta_localidad = 'BARRIOS UNIDOS'
